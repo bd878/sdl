5785276866d4e5769457ba44e76dbdf31b0d8142
teralion <slavad878@gmail.com>
2023-03-09 09:51:55 +0300

    gamepad and joystick;

--- 02/main.cpp
+++ 02/main.cpp
@@ -8,68 +8,53 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-const int BUTTON_WIDTH = 300;
-const int BUTTON_HEIGHT = 200;
-const int TOTAL_BUTTONS = 54;
+//Analog joystick dead zone
+const int JOYSTICK_DEAD_ZONE = 8000;
 
-enum LButtonSprite {
-  BUTTON_SPRITE_MOUSE_OUT = 0,
-  BUTTON_SPRITE_MOUSE_OVER_MOTION = 1,
-  BUTTON_SPRITE_MOUSE_DOWN = 2,
-  BUTTON_SPRITE_MOUSE_UP = 3,
-  BUTTON_SPRITE_TOTAL = 4
-};
+//Texture wrapper class
+class LTexture
+{
+  public:
+    //Initializes variables
+    LTexture();
 
-class LTexture {
-public:
-  // initializes variables
-  LTexture();
+    //Deallocates memory
+    ~LTexture();
 
-  // deallocates memory
-  ~LTexture();
+    //Loads image at specified path
+    bool loadFromFile( std::string path );
+    
+    #if defined(SDL_TTF_MAJOR_VERSION)
+    //Creates image from font string
+    bool loadFromRenderedText( std::string textureText, SDL_Color textColor );
+    #endif
 
-  // loads image from path
-  bool loadFromFile(std::string path);
+    //Deallocates texture
+    void free();
 
-#if defined(SDL_TTF_MAJOR_VERSION)
-  bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
-#endif
+    //Set color modulation
+    void setColor( Uint8 red, Uint8 green, Uint8 blue );
 
-  // deallocates texture
-  void free();
+    //Set blending
+    void setBlendMode( SDL_BlendMode blending );
 
-  void setColor(Uint8 red, Uint8 green, Uint8 blue);
+    //Set alpha modulation
+    void setAlpha( Uint8 alpha );
+    
+    //Renders texture at given point
+    void render( int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE );
 
-  void setBlendMode(SDL_BlendMode blending);
+    //Gets image dimensions
+    int getWidth();
+    int getHeight();
 
-  void setAlpha(Uint8 alpha);
+  private:
+    //The actual hardware texture
+    SDL_Texture* mTexture;
 
-  // render texture at given point
-  void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
-
-  int getWidth();
-  int getHeight();
-
-private:
-  SDL_Texture* mTexture;
-
-  int mWidth;
-  int mHeight;
-};
-
-class LButton {
-public:
-  LButton();
-
-  void setPosition(int x, int y);
-
-  void handleEvent(SDL_Event* e);
-
-  void render();
-private:
-  SDL_Point mPosition;
-
-  LButtonSprite mCurrentSprite;
+    //Image dimensions
+    int mWidth;
+    int mHeight;
 };
 
 //Starts up SDL and creates window
@@ -83,312 +68,390 @@ void close();
 
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
-  
-//The surface contained by the window
+
+//The window renderer
 SDL_Renderer* gRenderer = NULL;
 
-TTF_Font* gFont = NULL;
+//Scene textures
+LTexture gArrowTexture;
 
+//Game Controller 1 handler
+SDL_Joystick* gGameController = NULL;
 
-//Mouse button sprites
-SDL_Rect gSpriteClips[ BUTTON_SPRITE_TOTAL ];
-LTexture gButtonSpriteSheetTexture;
+//Globally used font
+TTF_Font *gFont = NULL;
 
-//Buttons objects
-LButton gButtons[ TOTAL_BUTTONS ]; 
-
-LTexture::LTexture() {
+LTexture::LTexture()
+{
+  //Initialize
   mTexture = NULL;
   mWidth = 0;
   mHeight = 0;
 }
 
-LTexture::~LTexture() {
+LTexture::~LTexture()
+{
+  //Deallocate
   free();
 }
 
-void LTexture::setColor(Uint8 red, Uint8 green, Uint8 blue) {
-  SDL_SetTextureColorMod(mTexture, red, green, blue);
-}
-
-bool LTexture::loadFromFile(std::string path) {
+bool LTexture::loadFromFile( std::string path )
+{
+  //Get rid of preexisting texture
   free();
 
+  //The final texture
   SDL_Texture* newTexture = NULL;
-  SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-  if (loadedSurface == NULL) {
-    printf("unable to load image %s! SDL_image error: %s\n",
-      path.c_str(), IMG_GetError());
-  } else {
-    SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
-    newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-    if (newTexture == NULL) {
-      printf("unable to create texture");
-    } else {
+
+  //Load image at specified path
+  SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+  if( loadedSurface == NULL )
+  {
+    printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+  }
+  else
+  {
+    //Color key image
+    SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+
+    //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+    if( newTexture == NULL )
+    {
+      printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+    }
+    else
+    {
+      //Get image dimensions
       mWidth = loadedSurface->w;
       mHeight = loadedSurface->h;
     }
 
-    SDL_FreeSurface(loadedSurface);
+    //Get rid of old loaded surface
+    SDL_FreeSurface( loadedSurface );
   }
 
+  //Return success
   mTexture = newTexture;
   return mTexture != NULL;
 }
 
 #if defined(SDL_TTF_MAJOR_VERSION)
-bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor) {
+bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor )
+{
+  //Get rid of preexisting texture
   free();
 
-  SDL_Surface* textSurface = TTF_RenderText_Solid(gFont, textureText.c_str(), textColor);
-  if (textSurface == NULL) {
-    printf("unable to render text surface!");
-  } else {
-    mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
-    if (mTexture == NULL) {
-      printf("unable to create texture");
-    } else {
+  //Render text surface
+  SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor );
+  if( textSurface != NULL )
+  {
+    //Create texture from surface pixels
+        mTexture = SDL_CreateTextureFromSurface( gRenderer, textSurface );
+    if( mTexture == NULL )
+    {
+      printf( "Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+      //Get image dimensions
       mWidth = textSurface->w;
       mHeight = textSurface->h;
     }
 
-    SDL_FreeSurface(textSurface);
+    //Get rid of old surface
+    SDL_FreeSurface( textSurface );
+  }
+  else
+  {
+    printf( "Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError() );
   }
 
+  
+  //Return success
   return mTexture != NULL;
 }
 #endif
 
-LButton::LButton() {
-  mPosition.x = 0;
-  mPosition.y = 0;
-
-  mCurrentSprite = BUTTON_SPRITE_MOUSE_UP;
-}
-
-void LButton::setPosition(int x, int y) {
-  mPosition.x = x;
-  mPosition.y = y;
-}
-
-void LButton::handleEvent(SDL_Event* e) {
-  if (e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP) {
-    int x, y;
-    SDL_GetMouseState(&x, &y);
-
-    bool inside = true;
-    if (x < mPosition.x) {
-      inside = false;
-    } else if (x > mPosition.x + BUTTON_WIDTH) {
-      inside = false;
-    } else if (y < mPosition.y) {
-      inside = false;
-    } else if (y > mPosition.y + BUTTON_HEIGHT) {
-      inside = false;
-    }
-
-    if (!inside) {
-      mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
-    } else {
-      switch (e->type) {
-        case SDL_MOUSEMOTION:
-        mCurrentSprite = BUTTON_SPRITE_MOUSE_OVER_MOTION;
-        break;
-
-        case SDL_MOUSEBUTTONDOWN:
-        mCurrentSprite = BUTTON_SPRITE_MOUSE_DOWN;
-        break;
-
-        case SDL_MOUSEBUTTONUP:
-        mCurrentSprite = BUTTON_SPRITE_MOUSE_UP;
-        break;
-      }
-    }
-  }
-}
-
-void LButton::render() {
-  gButtonSpriteSheetTexture.render(mPosition.x, mPosition.y, &gSpriteClips[mCurrentSprite]);
-}
-
-void LTexture::free() {
-  if (mTexture != NULL) {
-    SDL_DestroyTexture(mTexture);
+void LTexture::free()
+{
+  //Free texture if it exists
+  if( mTexture != NULL )
+  {
+    SDL_DestroyTexture( mTexture );
     mTexture = NULL;
     mWidth = 0;
     mHeight = 0;
   }
 }
 
-void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip) {
-  SDL_Rect renderQuad = { x, y, mWidth, mHeight};
+void LTexture::setColor( Uint8 red, Uint8 green, Uint8 blue )
+{
+  //Modulate texture rgb
+  SDL_SetTextureColorMod( mTexture, red, green, blue );
+}
 
-  if (clip != NULL) {
+void LTexture::setBlendMode( SDL_BlendMode blending )
+{
+  //Set blending function
+  SDL_SetTextureBlendMode( mTexture, blending );
+}
+    
+void LTexture::setAlpha( Uint8 alpha )
+{
+  //Modulate texture alpha
+  SDL_SetTextureAlphaMod( mTexture, alpha );
+}
+
+void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
+{
+  //Set rendering space and render to screen
+  SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+
+  //Set clip rendering dimensions
+  if( clip != NULL )
+  {
     renderQuad.w = clip->w;
     renderQuad.h = clip->h;
   }
 
-  SDL_RenderCopyEx(gRenderer, mTexture, clip, &renderQuad, angle, center, flip);
+  //Render to screen
+  SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip );
 }
 
-int LTexture::getWidth() {
+int LTexture::getWidth()
+{
   return mWidth;
 }
 
-int LTexture::getHeight() {
+int LTexture::getHeight()
+{
   return mHeight;
-}
-
-void LTexture::setBlendMode(SDL_BlendMode blending) {
-  SDL_SetTextureBlendMode(mTexture, blending);
-}
-
-void LTexture::setAlpha(Uint8 alpha) {
-  SDL_SetTextureAlphaMod(mTexture, alpha);
 }
 
 bool init()
 {
+  //Initialization flag
   bool success = true;
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  //Initialize SDL
+  if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_JOYSTICK ) < 0 )
   {
-    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
     success = false;
   }
   else
   {
-    if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
-      printf("Warning: linear texture filtering not enabled");
+    //Set texture filtering to linear
+    if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+    {
+      printf( "Warning: Linear texture filtering not enabled!" );
     }
 
-    gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (gWindow == NULL)
+    //Check for joysticks
+    if( SDL_NumJoysticks() < 1 )
     {
-      printf("Window could not be created! SDL Error %s\n", SDL_GetError());
+      printf( "Warning: No joysticks connected!\n" );
     }
     else
     {
-      gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-      if (gRenderer == NULL) {
-        printf("renderer could not be created: %s\n", SDL_GetError());
+      //Load joystick
+      gGameController = SDL_JoystickOpen( 0 );
+      if( gGameController == NULL )
+      {
+        printf( "Warning: Unable to open game controller! SDL Error: %s\n", SDL_GetError() );
+      }
+    }
+
+    //Create window
+    gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( gWindow == NULL )
+    {
+      printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+      success = false;
+    }
+    else
+    {
+      //Create vsynced renderer for window
+      gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+      if( gRenderer == NULL )
+      {
+        printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
         success = false;
-      } else {
-        // init renderer color
-        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+      }
+      else
+      {
+        //Initialize renderer color
+        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 
-        // init png loading
+        //Initialize PNG loading
         int imgFlags = IMG_INIT_PNG;
-        if (!(IMG_Init(imgFlags) & imgFlags)) {
-          printf("SDL_Image could not initialize!\n");
-          success = false;
-        }
-
-        if( TTF_Init() == -1 )
+        if( !( IMG_Init( imgFlags ) & imgFlags ) )
         {
-          printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
+          printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
           success = false;
         }
+      }
+
+       //Initialize SDL_ttf
+      if( TTF_Init() == -1 )
+      {
+        printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
+        success = false;
       }
     }
   }
 
+  return success;
+}
+
+bool loadMedia()
+{
+  //Loading success flag
+  bool success = true;
+
+  gFont = TTF_OpenFont( "lazy.ttf", 28 );
+  if( gFont == NULL )
+  {
+    printf( "Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError() );
+    success = false;
+  }
+
+  //Load arrow texture
+  if( !gArrowTexture.loadFromFile( "arrow.png" ) )
+  {
+    printf( "Failed to load arrow texture!\n" );
+    success = false;
+  }
+  
   return success;
 }
 
 void close()
 {
-  TTF_CloseFont(gFont);
-  gFont = NULL;
+  //Free loaded images
+  gArrowTexture.free();
 
-  SDL_DestroyRenderer(gRenderer);
-  SDL_DestroyWindow(gWindow);
+  //Close game controller
+  SDL_JoystickClose( gGameController );
+  gGameController = NULL;
+
+  //Destroy window  
+  SDL_DestroyRenderer( gRenderer );
+  SDL_DestroyWindow( gWindow );
   gWindow = NULL;
   gRenderer = NULL;
 
+  //Quit SDL subsystems
   IMG_Quit();
   SDL_Quit();
 }
 
-bool loadMedia() {
-  bool success = true;
-
-  //Load sprites
-  if( !gButtonSpriteSheetTexture.loadFromFile( "button.png" ) )
-  {
-    printf( "Failed to load button sprite texture!\n" );
-    success = false;
-  }
-  else
-  {
-    //Set sprites
-    for( int i = 0; i < BUTTON_SPRITE_TOTAL; ++i )
-    {
-      gSpriteClips[ i ].x = 0;
-      gSpriteClips[ i ].y = i * 200;
-      gSpriteClips[ i ].w = BUTTON_WIDTH;
-      gSpriteClips[ i ].h = BUTTON_HEIGHT;
-    }
-
-    //Set buttons in corners
-    gButtons[ 0 ].setPosition( 0, 0 );
-    gButtons[ 1 ].setPosition( SCREEN_WIDTH - BUTTON_WIDTH, 0 );
-    gButtons[ 2 ].setPosition( 0, SCREEN_HEIGHT - BUTTON_HEIGHT );
-    gButtons[ 3 ].setPosition( SCREEN_WIDTH - BUTTON_WIDTH, SCREEN_HEIGHT - BUTTON_HEIGHT );
-  }
-
-  return success;
-}
-
-int main(int argc, char* argv[])
+int main( int argc, char* args[] )
 {
-  if (!init())
+  //Start up SDL and create window
+  if( !init() )
   {
-    printf("failed to initialize\n");
+    printf( "Failed to initialize!\n" );
   }
   else
   {
-    if (!loadMedia())
+    //Load media
+    if( !loadMedia() )
     {
-      printf("Failed ot load media\n");
+      printf( "Failed to load media!\n" );
     }
     else
-    {
+    { 
+      //Main loop flag
       bool quit = false;
 
+      //Event handler
       SDL_Event e;
 
-      double degrees = 0;
+      //Normalized direction
+      int xDir = 0;
+      int yDir = 0;
 
-      SDL_RendererFlip flipType = SDL_FLIP_NONE;
-
-      int frame = 0;
-
-      Uint8 a = 255;
-
-      while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-          if (e.type == SDL_QUIT) {
+      //While application is running
+      while( !quit )
+      {
+        //Handle events on queue
+        while( SDL_PollEvent( &e ) != 0 )
+        {
+          //User requests quit
+          if( e.type == SDL_QUIT )
+          {
             quit = true;
           }
-
-          for (int i = 0; i < TOTAL_BUTTONS; ++i) {
-            gButtons[i].handleEvent(&e);
+          else if( e.type == SDL_JOYAXISMOTION )
+          {
+            //Motion on controller 0
+            if( e.jaxis.which == 0 )
+            {           
+              //X axis motion
+              if( e.jaxis.axis == 0 )
+              {
+                //Left of dead zone
+                if( e.jaxis.value < -JOYSTICK_DEAD_ZONE )
+                {
+                  xDir = -1;
+                }
+                //Right of dead zone
+                else if( e.jaxis.value > JOYSTICK_DEAD_ZONE )
+                {
+                  xDir =  1;
+                }
+                else
+                {
+                  xDir = 0;
+                }
+              }
+              //Y axis motion
+              else if( e.jaxis.axis == 1 )
+              {
+                //Below of dead zone
+                if( e.jaxis.value < -JOYSTICK_DEAD_ZONE )
+                {
+                  yDir = -1;
+                }
+                //Above of dead zone
+                else if( e.jaxis.value > JOYSTICK_DEAD_ZONE )
+                {
+                  yDir =  1;
+                }
+                else
+                {
+                  yDir = 0;
+                }
+              }
+            }
           }
         }
 
+        //Clear screen
         SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
         SDL_RenderClear( gRenderer );
 
-        for (int i = 0; i < TOTAL_BUTTONS; ++i) {
-          gButtons[i].render();
+        //Calculate angle
+        double joystickAngle = atan2( (double)yDir, (double)xDir ) * ( 180.0 / M_PI );
+        
+        //Correct angle
+        if( xDir == 0 && yDir == 0 )
+        {
+          joystickAngle = 0;
         }
 
+        //Render joystick 8 way angle
+        gArrowTexture.render( ( SCREEN_WIDTH - gArrowTexture.getWidth() ) / 2, ( SCREEN_HEIGHT - gArrowTexture.getHeight() ) / 2, NULL, joystickAngle );
+
+        //Update screen
         SDL_RenderPresent( gRenderer );
       }
     }
   }
 
+  //Free resources and close SDL
   close();
 
   return 0;
